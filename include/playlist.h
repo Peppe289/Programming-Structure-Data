036c323e8f7139c7e55d54e8bee018f25e449ab9
Peppe289 <gsperanza204@gmail.com>
2023-04-02 23:28:10 +0200

    remove unused function
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- include/playlist.h
+++ include/playlist.h
@@ -7,9 +7,6 @@ typedef struct playlist *Playlist;
 
 Playlist createPlaylist(char *name);
 void addSong(Playlist, Song);
-void removeSong(Playlist, char *);
-void sortPlaylist(Playlist);
-void printPlaylist(Playlist);
 
 List songList(Playlist data);
 void *findName(List data, char *name);
